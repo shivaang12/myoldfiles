9f7ed72b086c090c57437543cc4a8747cf41f462
Shivang Patel <shivaang14@gmail.com>
2015-11-23 15:25:01 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+#MAIN DECLARATION
+
+import twitter, time, sys
+import telegram
+
+api = twitter.Api(consumer_key='c5RCiZmC2yl6giIqtovEFYOdV', consumer_secret='cxw1C9fxFhbyizCgyymcNXTgfIZ631Xfj0DYjjr4eNKK5d3F9D', access_token_key='1937280930-Mz5dD3z3AfL1YH3kF7K7DVK1bvFtIJdRnrArWMP', access_token_secret='K32tCxA356Rpd6yCnWA8Bw8f6LohwPyNY2AcM8Yfy80R4')
+
+bot = telegram.Bot(token="144762386:AAGA1ElcWoFvE-MEL3zT7DaYaZCUT3NKbnI") #bot name jarvis
+
+
+update = bot.getUpdates()
+print update
+id = update[0].message.chat_id
+
+#functions
+
+def getMyTimeLine():
+    return api.GetHomeTimeline(count=1)
+
+def textMessage(name, text1):
+    Name = "*"+name+"*"
+    final = Name+" - "+"_"+text1+"_"
+    bot.sendMessage(chat_id=id, text=final, parse_mode=telegram.ParseMode.MARKDOWN)
+
+NAME_HOLDER = ""
+TEXT_HOLDER = ""
+NAME_HOLDER_t = ""
+TEXT_HOLDER_t = ""
+
+while 1:
+    #to catch a tweet
+    ins = getMyTimeLine()
+
+    #There parms will hold out data
+
+    for s in ins:
+        print s.user.name
+        NAME_HOLDER = s.user.name
+        print s.text
+        TEXT_HOLDER = s.text
+
+    if (TEXT_HOLDER != TEXT_HOLDER_t):
+        textMessage(NAME_HOLDER, TEXT_HOLDER)
+
+    NAME_HOLDER_t = NAME_HOLDER
+    TEXT_HOLDER_t = TEXT_HOLDER
+    time.sleep(60)
